ca7e03d83c5e30bf8f7e76aad1b2fea9c2e9f246
Tamal Saha <tamal@appscode.com>
2017-12-18 02:54:35 -0500

    added-all

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"github.com/appscode/go/log"
-	crd_cs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"github.com/tamalsaha/go-oneliners"
-	"k8s.io/client-go/util/homedir"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"net"
 	"path/filepath"
+	"sort"
+
+	"github.com/appscode/go/log"
+	"github.com/tamalsaha/go-oneliners"
+	core "k8s.io/api/core/v1"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	cc "k8s.io/client-go/kubernetes/typed/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
+	i1 := net.ParseIP("192.168.100.103").To4()
+	i2 := net.ParseIP("192.168.100.102").To4()
+
+	ips := []net.IP{i1, i2}
+	sort.Slice(ips, func(i, j int) bool { return bytes.Compare(ips[i], ips[j]) < 0 })
+	for _, ip := range ips {
+		oneliners.FILE(ip.String())
+	}
+
 	masterURL := ""
 	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube/config")
 
@@ -22,22 +40,80 @@ func main() {
 	}
 
 	kc := kubernetes.NewForConfigOrDie(config)
-	nodes, err := kc.CoreV1().Nodes().List(metav1.ListOptions{})
+	oneliners.FILE(main2(kc.CoreV1().Nodes()))
+}
+
+func main2(kc cc.NodeInterface) string {
+	nodes, err := kc.List(metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(map[string]string{
+			"node-role.kubernetes.io/master": "",
+		}).String(),
+	})
 	if err != nil {
-		log.Fatal(err)
+		return reasonForError(err)
 	}
-	if nodes != nil {
-		for _, node := range nodes.Items {
-			oneliners.FILE(node.Name)
+	if len(nodes.Items) == 0 {
+		nodes, err = kc.List(metav1.ListOptions{
+			LabelSelector: labels.SelectorFromSet(map[string]string{
+				"kubernetes.io/hostname": "minikube",
+			}).String(),
+		})
+		if err != nil {
+			return reasonForError(err)
 		}
 	}
 
-	crdClient := crd_cs.NewForConfigOrDie(config)
-	crds, err := crdClient.ApiextensionsV1beta1().CustomResourceDefinitions().List(metav1.ListOptions{})
-	if err != nil {
-		log.Fatal(err)
+	ips := make([]net.IP, 0, len(nodes.Items))
+	for _, node := range nodes.Items {
+		ip := nodeIP(node)
+		if ip != nil {
+			ips = append(ips, ip)
+		}
 	}
-	for _, crd := range crds.Items {
-		oneliners.FILE(crd.Name)
+	if len(ips) == 0 {
+		return ""
 	}
+	sort.Slice(ips, func(i, j int) bool { return bytes.Compare(ips[i], ips[j]) < 0 })
+	hasher := md5.New()
+	for _, ip := range ips {
+		hasher.Write(ip)
+	}
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func nodeIP(node core.Node) []byte {
+	for _, addr := range node.Status.Addresses {
+		if addr.Type == core.NodeExternalIP {
+			return ipBytes(net.ParseIP(addr.Address))
+		}
+	}
+	for _, addr := range node.Status.Addresses {
+		if addr.Type == core.NodeInternalIP {
+			return ipBytes(net.ParseIP(addr.Address))
+		}
+	}
+	return nil
+}
+
+func ipBytes(ip net.IP) []byte {
+	if ip == nil {
+		return nil
+	}
+	v4 := ip.To4()
+	if v4 != nil {
+		return v4
+	}
+	v6 := ip.To16()
+	if v6 != nil {
+		return v6
+	}
+	return nil
+}
+
+func reasonForError(err error) string {
+	switch t := err.(type) {
+	case kerr.APIStatus:
+		return string(t.Status().Reason)
+	}
+	return string(metav1.StatusReasonUnknown)
 }
